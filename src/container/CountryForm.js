bf50cd8b5efeb1f053ec2dea24a1ca3f8e546876
armani <razzarioa@gmail.com>
2019-02-24 11:55:42 +0600

    Added idea to gitignore, cleaned App.js, created container CountryForm.js

---
+++ src/container/CountryForm.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import "./CountryForm.css";
+
+class CountryForm extends Component {
+
+
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
